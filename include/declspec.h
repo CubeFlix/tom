aa6afd34cd6b1abfb1507832bae04b4b3bed7a25
Kevin Chen <kevin.signal@gmail.com>
2023-02-06 10:20:40 -0600

    fixed declspec

--- include/declspec.h
+++ include/declspec.h
@@ -4,10 +4,14 @@
 #ifndef DECLSPEC_H
 #define DECLSPEC_H
 
+#ifdef _MSC_VER
 #ifdef TOM_EXPORTS
 #define TOM_API __declspec(dllexport)
 #else
 #define TOM_API __declspec(dllimport)
+#endif
+#else
+#define TOM_API
 #endif
 
 #endif
